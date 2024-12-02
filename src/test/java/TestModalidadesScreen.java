00023467bd99062f618e57dc8b35597f00aeda45
Waleska Oliveira <waleska.ziviani.peixoto@gmail.com>
2024-12-02 16:28:15 -0300

    refactor(test): refactor TestModalidadesScreen class and add Should show error message when inserting a negative number in the inclusion modalidade

--- src/test/java/TestModalidadesScreen.java
+++ src/test/java/TestModalidadesScreen.java
@@ -1,4 +1,6 @@
+import com.github.javafaker.Faker;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,20 +12,34 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Log4j2
 public class TestModalidadesScreen {
     public static final String LOCALDRIVER = "src/main/resources/drivers/chromedriver.exe";
     public static final String PROPERTY = "webdriver.chrome.driver";
-    public static final String URL = "https://ciscodeto.github.io/AcodemiaGerenciamento/yalunos.html";
+    public static final String URL = "https://ciscodeto.github.io/AcodemiaGerenciamento/ymodalidades.html";
     public static final int WAITTIME = 7;
     public static final String XPATH_MENSAGEM = "/html/body/div[1]/div/p";
     public static final String XPATH_BUTTON_INSERT = "/html/body/div[2]/main/div[1]/ul/li[3]/a";
     public static final String CONTAINERDEAVISO = "/html/body/div[1]/div";
+    public static final String XPATH_INPUT_FIELD ="/html/body/div[2]/main/div[1]/ul/li[1]/input";
+    public static final String XPATH_BUTTON_FINALIZAR = "/html/body/div[1]/div/form/div[4]/a[2]";
+    public static final String XPATH_BUTTON_LISTAR = "/html/body/div[2]/main/div[1]/ul/li[2]/a";
+    public static final String XPATH_CONTENTTABLE = "/html/body/div[2]/main/div[2]";
+    Faker faker = new Faker();
 
     private WebDriver driver;
     ChromeOptions chromeOptions = new ChromeOptions();
+
+    public WebElement getWebElement( WebDriver driver,String xPath) {
+        new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(xPath)));
+        return driver.findElement(By.xpath(xPath));
+    }
 
     @BeforeEach
     void setUp() {
@@ -45,6 +61,29 @@ public class TestModalidadesScreen {
         final String mensagemDevolvida = container.findElement(By.xpath(XPATH_MENSAGEM)).getText();
         System.out.println(mensagemDevolvida);
     }
+
+    @Test
+    @DisplayName("Should show error message when inserting a negative number in the inclusion modalidade")
+    public void shouldShowErrorForNegativeNumber() throws InterruptedException {
+        log.info("Accessing site");
+        driver.get(URL);
+        final WebElement inputField = new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_INPUT_FIELD)));
+        String codigo = faker.numerify("-###");
+        inputField.sendKeys(codigo);
+        new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
+                .until(in -> codigo.equals(inputField.getAttribute("value")));
+        final WebElement button = new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(XPATH_BUTTON_INSERT)));
+        button.click();
+        final WebElement buttonFinalizar = getWebElement(driver,XPATH_BUTTON_FINALIZAR);
+        buttonFinalizar.click();
+        final WebElement buttonListar = getWebElement(driver, XPATH_BUTTON_LISTAR);
+        buttonListar.click();
+        final WebElement contentTable = getWebElement(driver, XPATH_CONTENTTABLE);
+        Assertions.fail("Não foi retornado mensagem sobre inserção de número negativo, segue os dados permitidos em cadastro: " + contentTable.getText());
+    }
+
 
     //@AfterEach
     //void tearDown() {
