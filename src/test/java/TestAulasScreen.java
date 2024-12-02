fc1eecccd13420513bc215b0bd032b161872502d
kaykyjuvenal <neguito.juvenal@gmail.com>
2024-12-01 21:37:51 -0300

    refactor(test): refactor TestAulasScreen class and add shouldOpenSiteAndClickListarWithoutFillCPFWithErrorReturn test

--- src/test/java/TestAulasScreen.java
+++ src/test/java/TestAulasScreen.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Log4j2
 public class TestAulasScreen {
-    public static final String CONTAINERDEAVISO = "/html/body/div[1]/div";
+    public static final String XPATH_CONTAINERDEAVISO = "/html/body/div[1]/div";
     public static final String LOCALDRIVER = "src/main/resources/drivers/chromedriver.exe";
     public static final String PROPERTY = "webdriver.chrome.driver";
     public static final String URL = "https://ciscodeto.github.io/AcodemiaGerenciamento/yalunos.html";
@@ -33,6 +33,12 @@ public class TestAulasScreen {
     Faker faker = new Faker();
     private WebDriver driver;
     ChromeOptions chromeOptions = new ChromeOptions();
+    public WebElement getWebElement( WebDriver driver,String xPath) {
+        new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(xPath)));
+        return driver.findElement(By.xpath(xPath));
+    }
+
 
     @BeforeEach
     void setUp() {
@@ -48,13 +54,11 @@ public class TestAulasScreen {
     public void shouldOpenSiteAndClickOnIncluirWithAInsertErrorMessage() {
 
             // Espera pelo botão e clica
-            final WebElement button = new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath(XPATH_BUTTON_INSERT)));
+            final WebElement button = getWebElement(driver, XPATH_BUTTON_INSERT);
             button.click();
 
             // Espera pelo container da mensagem
-            final WebElement container = new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath(CONTAINERDEAVISO)));
+            final WebElement container = getWebElement(driver, XPATH_CONTAINERDEAVISO);
             String returnedText = container.findElement(By.xpath(XPATH_MENSAGEM)).getText();
             container.findElement(By.xpath(XPATH_BOTAO_FECHAR)).click();
             // Verifica a mensagem retornada
@@ -65,26 +69,32 @@ public class TestAulasScreen {
     @Test
     @DisplayName("Should open Site and don't fill user infos, with error return")
     public void shouldOpenSiteAndDontFillUserInfosWithErrorReturn() {
-        final WebElement input = new WebDriverWait(driver, Duration.ofSeconds(WAITTIME)).
-                until(ExpectedConditions.elementToBeClickable(By.xpath(XPATH_INSERIR_CPF)));
+        final WebElement input = getWebElement(driver,XPATH_INSERIR_CPF);
         String cpf = faker.numerify("###.###.###-##");
         input.sendKeys(cpf);
         new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
                 .until(in -> cpf.equals(input.getAttribute("value")));
-        final WebElement buttonIncluir = new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
-                .until(ExpectedConditions.elementToBeClickable(By.xpath(XPATH_BUTTON_INSERT)));
+        final WebElement buttonIncluir = getWebElement(driver,XPATH_BUTTON_INSERT);
         buttonIncluir.click();
-        final WebElement buttonFinalizar = new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
-                .until(ExpectedConditions.elementToBeClickable(By.xpath(XPATH_BUTTON_FINALIZAR)));
+        final WebElement buttonFinalizar = getWebElement(driver,XPATH_BUTTON_FINALIZAR);
         buttonFinalizar.click();
-        final WebElement buttonListar = new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
-                .until(ExpectedConditions.elementToBeClickable(By.xpath(XPATH_BUTTON_LISTAR)));
+        final WebElement buttonListar = getWebElement(driver,XPATH_BUTTON_LISTAR);
         buttonListar.click();
-        final WebElement contentTable = new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
-                .until(ExpectedConditions.elementToBeClickable(By.xpath(XPATH_CONTENTTABLE)));
+        final WebElement contentTable = getWebElement(driver,XPATH_CONTENTTABLE);
         Assertions.fail("Não foi retornado o erro, segue os dados permitidos em cadastro: " + contentTable.getText());
     }
-
+    @Test
+    @DisplayName("Should open Site and don't fill CPF, with error return")
+    public void shouldOpenSiteAndClickListarWithoutFillCPFWithErrorReturn() {
+        final WebElement buttonListar = getWebElement(driver,XPATH_BUTTON_LISTAR);
+        buttonListar.click();
+        try{
+            // Espera pelo container da mensagem
+            final WebElement container = getWebElement(driver,XPATH_CONTAINERDEAVISO);
+        }catch (Exception e){
+            Assertions.fail("A mensagem de aviso não foi gerada!");
+        }
+    }
 
     //@AfterEach
     //void tearDown() {
