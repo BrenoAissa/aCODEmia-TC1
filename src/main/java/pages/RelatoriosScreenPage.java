8c8cbba61cf9c376924695bb57c73b0d00003f3d
brenoaissa <brenoaugusto05@hotmail.com.br>
2024-12-04 03:27:57 -0300

    create: RelatoriosScreenPage class

---
+++ src/main/java/pages/RelatoriosScreenPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class RelatoriosScreenPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private final By cpfField = By.id("buscador-cpf");
+}
