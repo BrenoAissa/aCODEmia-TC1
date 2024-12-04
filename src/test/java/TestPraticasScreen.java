ac5867d98f51dd832832306ec13f0cf25cbbdab8
brenoaissa <brenoaugusto05@hotmail.com.br>
2024-12-04 00:10:34 -0300

    test: add test for listing without CPF input, expecting error message
    - Implemented a test to verify that when the user clicks the "listar" button without filling the CPF, an error message is triggered.
    - Used a try-catch block to check if the error message container is displayed and fail the test if not.

--- src/test/java/TestPraticasScreen.java
+++ src/test/java/TestPraticasScreen.java
@@ -1,9 +1,9 @@
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,37 +11,59 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import com.github.javafaker.Faker;
 
 import java.time.Duration;
 
 @Log4j2
 public class TestPraticasScreen {
-    public static final String CONTAINERDEAVISO = "/html/body/div[1]/div";
     public static final String LOCALDRIVER = "src/main/resources/drivers/chromedriver.exe";
     public static final String PROPERTY = "webdriver.chrome.driver";
     public static final String URL = "https://ciscodeto.github.io/AcodemiaGerenciamento/ypraticas.html";
     public static final int WAITTIME = 7;
-    public static final String XPATH_MENSAGEM = "/html/body/div[1]/div/p";
-    public static final String XPATH_BUTTON_INSERT = "/html/body/div[2]/main/div[1]/ul/li[4]/a";
-
+    Faker faker = new Faker();
     private WebDriver driver;
     ChromeOptions chromeOptions = new ChromeOptions();
+    public WebElement getWebElement( WebDriver driver,String xPath) {
+        return new WebDriverWait(driver, Duration.ofSeconds(WAITTIME))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(xPath)));
+    }
 
     @BeforeEach
     void setUp() {
         System.setProperty(PROPERTY, LOCALDRIVER);
         chromeOptions.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(chromeOptions);
-    }
-
-    @Test
-    @DisplayName("Should open Site and click on Incluir return User not exist")
-    public void shouldOpenSite() throws InterruptedException {
         log.info("Accessing site");
         driver.get(URL); // Acessa o site Acodemia
     }
+
+    @Test
+    @DisplayName("Should open Site and don't fill CPF, with error return")
+    public void shouldOpenSiteAndClickListarWithoutFillCPFWithErrorReturn() {
+        final WebElement buttonListar = driver.findElement(By.id("listar-pratica"));
+        buttonListar.click();
+        try{
+            // Espera pelo container da mensagem
+            isElementPresent(driver, By.id("modal-conteudo"));
+        }catch (Exception e){
+            Assertions.fail("A mensagem de aviso não foi gerada!");
+        }
+    }
+
+    private boolean isElementPresent(WebDriver driver, By locator) {
+        try {
+            driver.findElement(locator);
+            return true; // O elemento está presente
+        } catch (NoSuchElementException e) {
+            return false; // O elemento não está presente
+        }
+
     /*@AfterEach
     void tearDown() {
     driver.quit(); // Encerra o driver e fecha o navegador
     }*/
+
+
+    }
 }
